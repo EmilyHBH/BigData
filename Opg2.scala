46535837c29975bc8962ce82be97fc63c0bc0a54
EmilyHBH <30571567+EmilyHBH@users.noreply.github.com>
2019-11-21 12:47:05 +0100

    Delete Opg2.scala

--- Opg2.scala
+++
@@ -1,8 +0,0 @@
-val file = sc.textFile("hdfs://localhost:9000/xmlhadoop/map.osm")
-
-val streets = file.
-	filter(line => line.contains("k=\"addr:street\"")).
-	map(word => (word.slice(26,word.length()-3), 1)).
-	reduceByKey(_+_)
-
-streets.saveAsTextFile("hdfs://localhost:9000/scala/opg2")
